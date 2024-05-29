dbd68a3f026975b2a402f3d44c22e79e7c7e1e87
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2024-05-29 22:00:59 +0500

    Update Logo.tsx

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -2,10 +2,15 @@ import Link from 'next/link';
 import { Props } from 'next/script';
 import React from 'react';
 
-const Logo = ({title,className} :Props) => {
+// Logo component to display a clickable logo with a title and optional class names for styling
+const Logo = ({ title, className }: Props) => {
     return (
         <div>
-         <Link href="/"><h1 className={`text-3xl font-bold p-6 ${className}`}>{title}</h1></Link>   
+            {/* Link component from Next.js for client-side navigation */}
+            <Link href="/">
+                {/* The title is rendered as an H1 element with dynamic class names for styling */}
+                <h1 className={`text-3xl font-bold p-6 ${className}`}>{title}</h1>
+            </Link>   
         </div>
     );
 }
