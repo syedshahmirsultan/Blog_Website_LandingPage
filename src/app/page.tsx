52ef70fd7c790024c23793b608e5b5a61a717a59
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2024-05-29 22:03:05 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,29 @@
-import {groq} from 'next-sanity'
-import Hero from './components/Hero'
+import { groq } from 'next-sanity';
+import Hero from './components/Hero';
 import { client } from '@/sanity/lib/client';
 import BlogContent from './components/BlogContent';
 
-const query = groq `*[_type =='post']{
+// Define the GROQ query to fetch all posts with author and categories data, ordered by creation date in ascending order
+const query = groq`*[_type == 'post']{
   ...,
-   author->,
-   categories[]-> 
-     } | order(_createdAt asc)`
+  author->,
+  categories[]->
+} | order(_createdAt asc)`;
 
-
+// Async function to fetch posts and render the home page
 export default async function Home() {
+  // Fetch posts from the Sanity client using the defined query
   const posts = await client.fetch(query);
-  return (
-<main>
-<Hero/>
-<BlogContent posts={posts}/>
-</main>
 
-  )
+  // Render the main content of the home page
+  return (
+    <main>
+      {/* Render the Hero component */}
+      <Hero />
+      {/* Render the BlogContent component with fetched posts */}
+      <BlogContent posts={posts} />
+    </main>
+  );
 }
+
    
